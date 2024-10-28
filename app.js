811ba547efc3a8d622fe9daac586a13ca67d024d
Tayyaba Rasheed <kaneezamna234@gmail.com>
2024-10-28 18:54:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+let name = prompt("Please enter your name");
+document.write( "Name :" ,name , "<br>");
+
+let Fathername = prompt("Please enter your father name ");
+document.write( "FatherName :",Fathername , "<br>");
+
+let gender = prompt("Please enter your gender ");
+document.write( "Gender :",gender , "<br>");
+
+let Rollno = prompt("Please enter your roll number ");
+document.write( "Roll No :",Rollno ,"<br>");
+
+let  Course = prompt("Please enter your course\n 1-Web and Mobile app develpment\n2-Digital marketing\n3-Graphic Designing\n4-Introducing to AI\n5-Cyber Security\n6-Microsoft power BI\n7-UI/UX d");
+
+let campus = prompt("Please enter your campus\n1-Numaish\n2-Muhammad Ali Society\n3-Malir\n4-Johar\n5-Nazimabad\n6-Gulshan");
+
+if (cousre =="Web and Mobile app development" || cousre == "Digital marketing"|| course == "Graphic Designing" || course =="UI/UX d"|| course == "Microsoft power BI" || course == "microsoft power bi" || course =="Cyber Security" || course == "cyber security" || course == "Introducing to AI" || course == "introducing to AI" || course == "graphic designing" || Ccourse == "WMA" || course == "GD") {
+      if(campus == "Nzimabad" || campus == "nazimabad" || camps == "malir"|| campus == "Malir" || campus == "gulshan" || campus == "Gulshan" || campus == "Numasish" || campus == "muhammad ali society" || campus == "Johar" || campus == "johar") {
+      alert("Congratulation ! you are enrolled in SMIT");
+      document.write("Congratulation ! you are enrolled in SMIT");
+
+}
+}
+else{
+    document.write(" sorry ! yor are not enrolled in SMIT");
+}
+
+
+
